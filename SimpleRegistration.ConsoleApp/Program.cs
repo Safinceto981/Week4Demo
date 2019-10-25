37f1297072301ba1d9d9107b06d503f030a286b2
Safinceto981 <safi_981@abv.bg>
2019-10-25 13:54:46 +0300

    initial commit

---
+++ SimpleRegistration.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleRegistration.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
